8571b135833cc51f0d4060561fd35fdb5b94ff14
Jonas Reich <mail@jonasreich.de>
2021-04-30 20:51:11 +0200

    git subrepo pull Plugins/OpenUnrealUtilities
    subrepo:
      subdir:   "Plugins/OpenUnrealUtilities"
      merged:   "034c1d1"
    upstream:
      origin:   "https://github.com/JonasReich/OpenUnrealUtilities"
      branch:   "develop"
      commit:   "034c1d1"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- Plugins/OpenUnrealUtilities/Source/OUURuntime/Public/Templates/InterfaceUtils.h
+++ Plugins/OpenUnrealUtilities/Source/OUURuntime/Public/Templates/InterfaceUtils.h
@@ -7,16 +7,23 @@
 /**
  * Call a function on a blueprint implementable interface object.
  * The object can be any of [UObject*, TScriptInterface, IInterface*].
- * Only safe after validation via CanCallInterface()!
+ * Only safe after validation of the InterfaceObject, e.g. via CanCallInterface()!
+ * @param InterfaceType: The IInterface type
+ * @param Function: The function member of the Interface you want to invoke
+ * @param InterfaceObject: An object pointer, interface pointer or script interface of the matching interface
  */
-#define CALL_INTERFACE(InterfaceClass, Function, InterfaceObject, ...) \
-InterfaceClass::Execute_ ## Function(GetInterfaceObject(InterfaceObject), ## __VA_ARGS__);
+#define CALL_INTERFACE(InterfaceType, Function, InterfaceObject, ...) \
+( \
+(OUU_Interface_Private::Ignore(&InterfaceType::Function)), \
+(InterfaceType::Execute_ ## Function(GetInterfaceObject(InterfaceObject), ##  __VA_ARGS__)) \
+)
 
-template<typename T>
+
+template <typename T>
 using TIsIInterface_T = typename TEnableIf<TIsIInterface<T>::Value>::Type;
 
 /** Get the underlying object from an interface so you can call Execute_* functions on it */
-template<typename T, typename = TIsIInterface_T<T>>
+template <typename T, typename = TIsIInterface_T<T>>
 UObject* GetInterfaceObject(T* InterfaceObject)
 {
 	return Cast<UObject>(InterfaceObject);
@@ -32,7 +39,7 @@ FORCEINLINE UObject* GetInterfaceObject(UObject* InterfaceObject)
 }
 
 /** Get the underlying object from an interface so you can call Execute_* functions on it */
-template<typename T, typename = TIsIInterface_T<T>>
+template <typename T, typename = TIsIInterface_T<T>>
 UObject* GetInterfaceObject(TScriptInterface<T> InterfaceObject)
 {
 	return InterfaceObject.GetObject();
@@ -46,16 +53,16 @@ UObject* GetInterfaceObject(TScriptInterface<T> InterfaceObject)
  * - IInterfaceType*
  * - TScriptInterface<T>
  */
-template<typename T>
+template <typename T>
 FORCEINLINE bool IsValidInterface(UObject* InterfaceObject)
 {
 	return IsValid(InterfaceObject) && InterfaceObject->GetClass()->ImplementsInterface(T::UClassType::StaticClass());
 }
 
- /**
-  * Validate a IInterface pointer's underlying UObject.
-  */
-template<typename T, typename = TIsIInterface_T<T>>
+/**
+ * Validate a IInterface pointer's underlying UObject.
+ */
+template <typename T, typename = TIsIInterface_T<T>>
 FORCEINLINE bool IsValidInterface(T* InterfaceObject)
 {
 	return IsValid(GetInterfaceObject(InterfaceObject));
@@ -65,22 +72,30 @@ FORCEINLINE bool IsValidInterface(T* InterfaceObject)
  * Validate a TScriptInterface's underlying UObject.
  * Also makes sure the interface pointer is set to a correct value.
  */
-template<typename T, typename = TIsIInterface_T<T>>
+template <typename T, typename = TIsIInterface_T<T>>
 FORCEINLINE bool IsValidInterface(TScriptInterface<T>& InterfaceObject)
 {
-	UObject* ObjectPoiner = InterfaceObject.GetObject();
-	bool bResult = IsValidInterface<T>(ObjectPoiner);
-	InterfaceObject.SetInterface(bResult ? Cast<T>(ObjectPoiner) : nullptr);
+	UObject* ObjectPointer = InterfaceObject.GetObject();
+	bool bResult = IsValidInterface<T>(ObjectPointer);
+	InterfaceObject.SetInterface(bResult ? Cast<T>(ObjectPointer) : nullptr);
 	return bResult;
 }
 
 /** Override to throw compile time error for using const TScriptInterface<T> or const TScriptInterface<T>& */
-template<typename T, typename = typename TIsIInterface_T<T>>
-FORCEINLINE bool IsValIsValidInterfaceid(const TScriptInterface<T>& InterfaceObject)
+template <typename T, typename = TIsIInterface_T<T>>
+FORCEINLINE bool IsValidInterface(const TScriptInterface<T>& InterfaceObject)
 {
 	static_assert(false, "You should not use const TScriptInterface<T>! Three reasons why:\n"
 		"\t- No memory benefit over passing by value\n"
 		"\t- const TScriptInterface<T> cannot be invalidated when it turns stale or corrected by IsValid() check\n"
 		"\t- No const safeness for pointer target, because const TScriptInterface<T> still allows calling non-const functions");
 	return false;
+}
+
+namespace OUU_Interface_Private
+{
+	/* Use this to let the compiler ignore what you have passed in */
+	template<typename... Ts>
+	void Ignore(Ts...) {} 
+	
 }
